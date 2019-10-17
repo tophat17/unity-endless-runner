fadf8400daedf8108854fa6782382f6528a03de5
tophat17 <40347885+tophat17@users.noreply.github.com>
2019-10-17 14:46:50 -0600

    movement

---
+++ Endless-Runner/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+    public float yIncrament;
+
+    public float speed;
+    public float maxHight;
+    public float minHight;
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHight){
+            targetPos = new Vector2(transform.position.x, transform.position.y + yIncrament);
+            
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - yIncrament);
+
+        }
+    }
+}
